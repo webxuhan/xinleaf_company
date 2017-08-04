ad42ec39465b3d494ddc9749c2b24e4ff21480df
jijianchun <549332732@qq.com>
2017-08-04 14:45:33 +0800

    test。

--- src/components/head.vue
+++ src/components/head.vue
@@ -1,8 +1,34 @@
 <template>
 <div id="head">
-	head
+	<el-row>
+		<el-col :span="12">
+			<div class="logo"></div>
+		</el-col>
+		<el-col :span="12">
+			<div class="phone">
+				<div class="p-top"></div>
+				<div class="p-bottom">
+					<el-input placeholder="请输入内容" icon="search" v-model="search" :on-icon-click="handleIconClick"></el-input>
+				</div>
+			</div>
+		</el-col>
+	</el-row>
 </div>	
 </template>
+<script>
+export default {
+  data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    handleIconClick(ev) {
+      console.log(this.search);
+    }
+  }
+}
+</script>
 <style>
 	
 </style>
