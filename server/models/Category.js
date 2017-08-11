dbc697d60dab7a21502b4d6c5dd2e1066d5bc5ac
webxuhan <xuha_715@163.com>
2017-08-11 15:12:02 +0800

    该更分类和商品表

--- server/models/Category.js
+++ server/models/Category.js
@@ -9,12 +9,12 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
 	_id : {
 		type : String,
-		unqiue : true,
+		unique : true,
 		'defualt' : shortid.genrate
 	},
 	category_name : String,		//商品分类名称
-	parent_id : { type : String, defualt : '0' }, 	//父id,默认为0，0为最高级
-	is_show : { type : Boolean, defualt : false }, 	//是否显示,默认不显示
+	parent_id : { type : String, default : '0' }, 	//父id,默认为0，0为最高级
+	is_show : { type : Boolean, default : false }, 	//是否显示,默认不显示
 	sort_order : { type: String, default: "0" } 	//分类排序,默认为0,0为升序，1为降序
 	
 });
@@ -22,3 +22,5 @@ const CategorySchema = new Schema({
 const Category = mongoose.model("Category",CategorySchema);
 
 module.exports = Category;
+
+
