6936e08bfaa97c0f1cb7452dc0dfe509c51952b7
jijianchun <549332732@qq.com>
2017-08-11 11:21:49 +0800

    增加表单验证

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,18 +2,18 @@
 <div>
 	<el-row class="reg-wrap">
 		<el-col :span="12" :offset="6">
-			<el-form ref="form" :model="form" label-width="100px">
-			  <el-form-item label="用户姓名：">
+			<el-form :model="form" :rules="rules" ref="form" label-width="100px" class="demo-ruleForm">
+			  <el-form-item label="用户姓名：" prop="name">
 			    <el-input v-model="form.name" placeholder="请输入用户姓名"></el-input>
 			  </el-form-item>
-			  <el-form-item label="用户密码：">
-			    <el-input type="password" v-model="form.password" placeholder="请输入用户密码"></el-input>
+			  <el-form-item label="用户密码：" prop="password">
+			    <el-input type="password" v-model="form.password" auto-complete="off" placeholder="请输入用户密码"></el-input>
 			  </el-form-item>
-			  <el-form-item label="重复密码：">
-			    <el-input type="password" v-model="form.repassword" placeholder="请重复输入密码"></el-input>
+			  <el-form-item label="重复密码：" prop="repassword">
+			    <el-input type="password" v-model="form.repassword" auto-complete="off" placeholder="请重复输入密码"></el-input>
 			  </el-form-item>
 			  <el-form-item>
-			    <el-button type="primary" @click="register">立即注册</el-button>
+			    <el-button type="primary" @click="register('form')">立即注册</el-button>
 			  </el-form-item>
 			</el-form>
 		</el-col>
@@ -28,19 +28,43 @@ data() {
       name: '',
       password: '',
       repassword: ''
-    }
+    },
+    rules:{
+		name: [
+			{required: true, message: '请输入用户名', trigger: 'blur'},
+			{min: 3,max: 10, message: '长度在3到10个字符', trigger: 'blur'}
+		],
+		password: [
+			{required: true, message: '请输入密码', trigger: 'blur'},
+			{min: 6,max: 20, message: '长度在6到20个字符', trigger: 'blur'}
+		],
+		repassword: [
+			{required: true, message: '请重复输入密码', trigger: 'blur'},
+			{min: 6,max: 20, message: '长度在6到20个字符', trigger: 'blur'}
+		]
+	}
   }
 },
 methods: {
-  register() {
+  register(formName) {
+  	this.$refs[formName].validate((valid) => {
+  		if(valid){
+  			
+  			this.$http.post('http://study.jameschun.cc/data.php',this.form).then(function(res){
+		    	if(res){
+		    		console.log(res.data);
+		    	}else{
+		    		console.log('error');
+		    	}
+		    });
+  			
+  		}else{
+  			console.log('error');
+  			return false;
+  		}
+  	});
 
-    this.$http.post('http://study.jameschun.cc/data.php',this.form).then(function(res){
-    	if(res){
-    		console.log(res.data);
-    	}else{
-    		console.log('error');
-    	}
-    });
+    
   }
 }
 }
