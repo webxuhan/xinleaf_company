515386d084ecfdc98436f39ba9e105eae7179623
健男-春 <549332732@qq.com>
2017-08-20 10:27:39 +0800

    add pics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,7 @@
   <nv-head></nv-head>
   <el-row>
     <el-col :span="24">
-      <div class="grid-content bg-purple-dark">
-        <el-carousel indicator-position="outside">
-          <el-carousel-item v-for="item in 5" :key="item">
-            <h3>{{ item }}</h3>
-          </el-carousel-item>
-        </el-carousel>
-      </div>
+      主体区域内容
     </el-col>
   </el-row>
   <nv-foot></nv-foot>
@@ -27,19 +21,5 @@ export default {
 </script>
 
 <style>
-.el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 150px;
-    margin: 0;
-  }
 
-  .el-carousel__item:nth-child(2n) {
-     background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-     background-color: #d3dce6;
-  }
 </style>
