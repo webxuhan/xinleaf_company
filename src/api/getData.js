5a45edb3f0ba0403b137978b9f03182aecbe0c15
健男-春 <549332732@qq.com>
2017-09-02 13:59:17 +0800

    api抽离

--- src/api/getData.js
+++ src/api/getData.js
@@ -12,8 +12,26 @@ export const login = data => fetch('/admin/login', data, 'POST');
 
 export const signout = () => fetch('/admin/signout');
 
+
+
 /**
  * 获取员工列表信息
  */
-export const getStaffList = () => fetch('/admin/getStaffList');
+export const StaffList = data => fetch('/admin/getStaffList', data, 'POST');
+
+/**
+ * 删除员工
+ */
+export const delStaff = data => fetch('/admin/delStaffById', data, 'POST');
+
+/**
+ * 编辑员工信息
+ */
+export const editStaff = data => fetch('/admin/editStaff', data, 'POST');
+
+/**
+ * 添加员工信息
+ */
+export const addStaff = data => fetch('/admin/addStaff', data, 'POST');
+
 
