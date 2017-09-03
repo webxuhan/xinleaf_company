ad70f4d8c1aa4dfecb21788a37222af334d49edd
健男-春 <549332732@qq.com>
2017-09-03 23:31:53 +0800

    test ue

--- src/views/admin/home.vue
+++ src/views/admin/home.vue
@@ -1,17 +1,54 @@
+
+
 <template>
-	<div>
-		<head-top></head-top>
-		home.vue
-	</div>
+  <div class="components-container">
+  	<head-top></head-top>
+    <div class="info">UE编辑器示例<br>需要使用编辑器时，调用UE公共组件即可。可设置填充内容defaultMsg，配置信息config(宽度和高度等)，可调用组件中获取内容的方法。支持页面内多次调用。</div>
+    <button @click="getUEContent()">获取内容</button>
+    <div class="editor-container">
+      <UE :defaultMsg=defaultMsg :config=config :id=ue1 ref="ue"></UE>
+      <UE :defaultMsg=defaultMsg :config=config :id=ue2 ref="ue2"></UE>
+    </div>
+  </div>
 </template>
+<style>
+  .info{
+    border-radius: 10px;
+    line-height: 20px;
+    padding: 10px;
+    margin: 10px;
+    background-color: #ffffff;
+  }
+</style>
 <script>
 	import headTop from '../../components/headTop';
-	export default {
-		components:{
-			headTop
-		}
-	}
+  import UE from '../../components/ue/ue.vue';
+  export default {
+    components: {UE,headTop},
+    data() {
+      return {
+        defaultMsg: '这里是UE测试',
+        config: {
+          initialFrameWidth: null,
+          initialFrameHeight: 350
+        },
+        ue1: "ue1", // 不同编辑器必须不同的id
+        ue2: "ue2"
+      }
+    },
+    methods: {
+      getUEContent() {
+        let content = this.$refs.ue.getUEContent(); // 调用子组件方法
+        this.$notify({
+          title: '获取成功，可在控制台查看！',
+          message: content,
+          type: 'success'
+        });
+        console.log(content)
+      }
+    }
+  };
 </script>
-<style>
-	
-</style>
+
+
+
